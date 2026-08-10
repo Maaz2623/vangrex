8beb87dbc95eb85df177977424b35f74b4f8d54a
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2026-08-10 19:10:32 +0530

    modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import HomePage from "@/features/home/components/home-page";
+import { UnRequireAuth } from "@/lib/auth-utils";
 
 const Page = async () => {
+  await UnRequireAuth();
   return <HomePage />;
 };
 
