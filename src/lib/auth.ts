3f9285e58524dc8cd2f80afde18963e94a34f205
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2026-09-21 14:28:24 +0530

    new file:   src/app/api/auth/[...all]/route.ts 	modified:   src/db/schema.ts 	new file:   src/lib/auth-client.ts 	modified:   src/lib/auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,8 +2,9 @@ import { db } from "@/db";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
-export const aut = betterAuth({
+export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
+
 });
